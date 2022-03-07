c383dc4e58e3df191ed77bdbc91837a55fed4c93
alexanderoke <alexander.oke@student.moringaschool.com>
2022-03-07 22:05:38 +0300

    add rgeister and login temolates

--- instagram/insta/views.py
+++ instagram/insta/views.py
@@ -2,5 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
+images=[
+  {
+    'Image':'one',
+    'comment':'is',
+  },
+  {
+    'Image2':'two',
+    'comment':'is',
+  }
+
+  ]
+
+
+
+
 def home(request):
-  return render(request,'insta/home.html',{'title':'title'})
+  context={
+    'images':images
+  }
+  return render(request,'insta/home.html',context)
+
+
+def register(request):
+  return render(request,'insta/register.html')
+
+def login(request):
+  return render(request,'insta/login')
