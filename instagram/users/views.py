2bf5761b9b0525752cd8a79be1a2c4d181a66e48
alexanderoke <alexander.oke@student.moringaschool.com>
2022-03-10 09:38:07 +0300

    add message tags for successful sign up

--- instagram/users/views.py
+++ instagram/users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
@@ -9,6 +9,8 @@ def register(request):
     form = UserCreationForm(request.POST)
     if form.is_valid():
       username =form.cleaned_data.get('username')
+      messages.success(request, f'Account successfully created for {username}!')
+      return redirect('')
   else:
     form =UserCreationForm()  
   return render(request,'users/register.html',{'form':form})
