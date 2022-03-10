ad7fc9061eef959e4e0af94bc76d0566675ad627
alexanderoke <alexander.oke@student.moringaschool.com>
2022-03-10 20:51:53 +0300

    login required decorator added

--- instagram/users/views.py
+++ instagram/users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-# from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegisterForm
 
@@ -24,7 +24,7 @@ def login(request):
   return render(request,'users/login.html')
 
 
-
+@login_required
 def profile(request):
   return render(request, 'users/profile.html')
 
