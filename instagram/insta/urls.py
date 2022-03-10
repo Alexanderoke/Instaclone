70628882d6ca4165c91035ed545e2a43909ebb5a
alexanderoke <alexander.oke@student.moringaschool.com>
2022-03-10 08:04:43 +0300

    add validation conditonals on registration form view

--- instagram/insta/urls.py
+++ instagram/insta/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
   path('', views.login, name='insta-login'),
-  path('register/',views.register, name='insta-register'),
+  # path('register/',views.register, name='insta-register'),
   path('home/',views.home, name='insta-home'),
   
   
